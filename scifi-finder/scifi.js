ebe6a9f8913c198c1c0fa7b9b785b3d64fa38fd6
jastr945 <polina.jastr@gmail.com>
2017-10-12 01:18:30 -0700

    Articles load as select menu changes

--- scifi-finder/scifi.js
+++ scifi-finder/scifi.js
@@ -14,28 +14,18 @@ $.ajax({
   }
 });
 
-// Using Wikipedia's API to extract several articles from the 'Science_fiction_genres' category
-$.ajax({
-    url: 'http://en.wikipedia.org/w/api.php?format=xml&action=query&prop=extracts&titles=Gothic_science_fiction&redirects=true',
+
+// Using Wikipedia API to load an article of a user's choice from the select menu
+$('.selectpicker').change(function () {
+  var textTitle = $('.selectpicker option:selected').text();
+  $.ajax({
+    url: "http://en.wikipedia.org/w/api.php?format=xml&action=query&prop=extracts&titles=" + textTitle + "&redirects=true",
     data: {
         format: 'json'
     },
     dataType: 'jsonp',
     type: 'get',
 }).done(function(data) {
-    var pageNumber = "3311692";
-    $("#mainContainer").append(JSON.stringify(data["query"]["pages"][pageNumber]["extract"]));
+    $("#article").html(JSON.stringify(data["query"]["pages"]));
 });
-
-
-$.ajax({
-    url: 'http://en.wikipedia.org/w/api.php?format=xml&action=query&prop=extracts&titles=Steampunk&redirects=true',
-    data: {
-        format: 'json'
-    },
-    dataType: 'jsonp',
-    type: 'get',
-}).done(function(data) {
-    var pageNumber = "27684";
-    $("#mainContainer").append(JSON.stringify(data["query"]["pages"][pageNumber]["extract"]));
 });
