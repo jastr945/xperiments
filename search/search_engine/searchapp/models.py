0e350ee18c2390b2a8097a4e80f858964a87e488
jastr945 <polina.jastr@gmail.com>
2017-10-20 18:13:38 -0700

    Added Article model

--- search/search_engine/searchapp/models.py
+++ search/search_engine/searchapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+def upload_image(instance, filename):
+    """Images will be uploaded to MEDIA_ROOT/<name>/<filename>"""
+
+    return '{title}/{filename}'.format(title=instance.title, filename=filename)
+
+
+class Article(models.Model):
+    """Contains basic information about each article."""
+
+    title = models.CharField(max_length=255, default='', null=True)
+    body = models.CharField(max_length=3000, default='', null=True)
+    date = models.DateField(blank=False, null=False)
+    source = models.CharField(max_length=255, default='', null=True)
+    img = models.FileField(upload_to=upload_image)
