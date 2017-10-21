5ef83b7eda2fcbdfe6b0c743b73b7d9f0cb77f9e
jastr945 <polina.jastr@gmail.com>
2017-10-20 18:59:12 -0700

    Edited the index view to reflect the Article model

--- search/search_engine/searchapp/views.py
+++ search/search_engine/searchapp/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from .models import Article
 
 # rendering the main page
 def index(request):
 
+    articles = Article.objects.all()
+
     context_dict = {
+        'articles': articles,
     }
     return render(request, 'searchapp/index.html', context_dict)
