0a68c2e91036f61ccbebc0df1d53043fea0d30e8
jastr945 <polina.jastr@gmail.com>
2017-11-07 19:44:40 -0800

    Added highlight tag to date and category field

--- search/search_engine/searchapp/templatetags/custom_tags.py
+++ search/search_engine/searchapp/templatetags/custom_tags.py
@@ -8,5 +8,6 @@ register = template.Library()
 # Creating a template tag for highlighting a search term in search results
 @register.filter(name='highlight')
 def highlight(text, word):
+    text = str(text)
     pattern = re.compile('{}'.format(esc(word)), re.IGNORECASE)
     return mark_safe(pattern.sub(r'<mark>{}</mark>'.format(word), text))
