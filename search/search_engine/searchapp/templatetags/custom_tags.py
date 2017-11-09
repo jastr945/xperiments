3f7d50ba00546d219c67ab71248d67772da56d86
jastr945 <polina.jastr@gmail.com>
2017-11-08 21:56:00 -0800

    Changed highlighting template tag function to highlight each word in a phrase

--- search/search_engine/searchapp/templatetags/custom_tags.py
+++ search/search_engine/searchapp/templatetags/custom_tags.py
@@ -1,13 +1,15 @@
 from django import template
 from django.utils.safestring import mark_safe
-from django.utils.html import conditional_escape as esc
 import re
 
 register = template.Library()
 
-# Creating a template tag for highlighting a search term in search results
+# Creating a custom template tag for highlighting matching search terms in search results
+
 @register.filter(name='highlight')
 def highlight(text, word):
+    words_list = re.sub(r'[.!,;?]', ' ', word).split()
     text = r'{}'.format(text)
-    pattern = re.compile('({})'.format(esc(word)), re.IGNORECASE)
-    return mark_safe(pattern.sub(r'<mark>\1</mark>', text))
+    replacement = "<mark>" + "\\1" + "</mark>"
+    result = re.sub("(" + "|".join(map(re.escape, words_list)) + ")", replacement, text, flags=re.I)
+    return mark_safe(result)
