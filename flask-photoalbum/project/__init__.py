427c773e640c443e6dca279c983c92f51044c0a3
jastr945 <polina.jastr@gmail.com>
2017-12-16 20:24:42 -0800

    Added dockerfile and docker-compose

--- flask-photoalbum/project/__init__.py
+++ flask-photoalbum/project/__init__.py
@@ -1,3 +1,5 @@
+import sys
+import os
 from flask import Flask, jsonify
 
 
@@ -5,7 +7,10 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 # set config
-app.config.from_object('project.config.DevelopmentConfig')
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
+
+print(app.config, file=sys.stderr)
 
 @app.route('/ping', methods=['GET'])
 def ping_pong():
