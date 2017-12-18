c67fb37447b964590f1da794fbd184fe96a9b819
jastr945 <polina.jastr@gmail.com>
2017-12-17 17:50:53 -0800

    Added tests of get method for the User model

--- flask-photoalbum/project/tests/test_users.py
+++ flask-photoalbum/project/tests/test_users.py
@@ -1,6 +1,15 @@
 import json
 
 from project.tests.base import BaseTestCase
+from project import db
+from project.api.models import User
+
+
+def add_user(username, email):
+    user = User(username=username, email=email)
+    db.session.add(user)
+    db.session.commit()
+    return user
 
 
 class TestUserService(BaseTestCase):
@@ -80,3 +89,52 @@ class TestUserService(BaseTestCase):
             self.assertIn(
                 'Sorry. That email already exists.', data['message'])
             self.assertIn('fail', data['status'])
+
+    def test_single_user(self):
+        """Ensure get single user behaves correctly."""
+        user = add_user('polina', 'polina.jastr@gmail.com')
+        with self.client:
+            response = self.client.get(f'/users/{user.id}')
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 200)
+            self.assertTrue('created_at' in data['data'])
+            self.assertIn('polina', data['data']['username'])
+            self.assertIn('polina.jastr@gmail.com', data['data']['email'])
+            self.assertIn('success', data['status'])
+
+    def test_single_user_no_id(self):
+        """Ensure error is thrown if an id is not provided."""
+        with self.client:
+            response = self.client.get('/users/blah')
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 404)
+            self.assertIn('User does not exist', data['message'])
+            self.assertIn('fail', data['status'])
+
+    def test_single_user_incorrect_id(self):
+        """Ensure error is thrown if the id does not exist."""
+        with self.client:
+            response = self.client.get('/users/999')
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 404)
+            self.assertIn('User does not exist', data['message'])
+            self.assertIn('fail', data['status'])
+
+    def test_all_users(self):
+        """Ensure get all users behaves correctly."""
+        add_user('polina', 'polina.jastr@gmail.com')
+        add_user('pofi', 'pofi@catsruletheworld.com')
+        with self.client:
+            response = self.client.get('/users')
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(len(data['data']['users']), 2)
+            self.assertTrue('created_at' in data['data']['users'][0])
+            self.assertTrue('created_at' in data['data']['users'][1])
+            self.assertIn('polina', data['data']['users'][0]['username'])
+            self.assertIn(
+                'polina.jastr@gmail.com', data['data']['users'][0]['email'])
+            self.assertIn('pofi', data['data']['users'][1]['username'])
+            self.assertIn(
+                'pofi@catsruletheworld.com', data['data']['users'][1]['email'])
+            self.assertIn('success', data['status'])
