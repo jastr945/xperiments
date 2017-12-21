214a3677f2efff5773f3d9e2976d4277142ae6d3
jastr945 <polina.jastr@gmail.com>
2017-12-20 19:30:24 -0800

    Added a form component to react

--- flask-photoalbum/client/src/index.js
+++ flask-photoalbum/client/src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 import UsersList from './components/UsersList';
+import AddUser from './components/AddUser';
 
 
 class App extends Component {
@@ -20,18 +21,20 @@ class App extends Component {
   .catch((err) => { console.log(err); })
 }
   render() {
-  return (
-    <div className="container">
-      <div className="row">
-        <div className="col-md-6">
-          <br/>
-          <h1>All Users</h1>
-          <hr/><br/>
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <br/>
+            <h1>All Users</h1>
+            <hr/><br/>
+            <AddUser/>
+            <br/>
             <UsersList users={this.state.users}/>
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
   }
 }
 
