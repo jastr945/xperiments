f90f66b15712f65a09c817301c3a61698a6d8b0d
jastr945 <polina.jastr@gmail.com>
2017-12-20 18:54:23 -0800

    Added UsersList component to react folder

--- flask-photoalbum/client/src/index.js
+++ flask-photoalbum/client/src/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
+import UsersList from './components/UsersList';
 
 
 class App extends Component {
@@ -18,24 +19,19 @@ class App extends Component {
   .then((res) => { this.setState({ users: res.data.data.users }); })
   .catch((err) => { console.log(err); })
 }
-
   render() {
-    return (
-      <div className="container">
-        <div className="row">
-          <div className="col-md-4">
-            <br/>
-            <h1>All Users</h1>
-            <hr/><br/>
-            {
-              this.state.users.map((user) => {
-                return <h4 key={user.id} className="well"><strong>{ user.username }</strong> - <em>{user.created_at}</em></h4>
-              })
-            }
-          </div>
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6">
+          <br/>
+          <h1>All Users</h1>
+          <hr/><br/>
+            <UsersList users={this.state.users}/>
         </div>
       </div>
-    )
+    </div>
+  )
   }
 }
 
