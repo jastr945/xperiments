03c14237f57a15f199bb36d55d98e25a7b6a0080
jastr945 <polina.jastr@gmail.com>
2017-12-20 22:27:36 -0800

    Flask and react working together

--- flask-photoalbum/client/src/index.js
+++ flask-photoalbum/client/src/index.js
@@ -18,7 +18,7 @@ class App extends Component {
     this.getUsers();
   }
   getUsers() {
-    axios.get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`)
+    axios.get('http://localhost:5001/users')
     .then((res) => { this.setState({ users: res.data.data.users }); })
     .catch((err) => { console.log(err); })
   }
@@ -28,7 +28,7 @@ class App extends Component {
       username: this.state.username,
       email: this.state.email
     }
-    axios.post(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`, data)
+    axios.post('http://localhost:5001/users', data)
     .then((res) => {
       this.getUsers();
       this.setState({ username: '', email: '' });
