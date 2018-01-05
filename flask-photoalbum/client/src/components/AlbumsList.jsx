920df2ae65654ef6a2a409e2f83f0bed0490b237
jastr945 <polina.jastr@gmail.com>
2018-01-04 18:17:40 -0800

    Added styles to the header of each album

--- flask-photoalbum/client/src/components/AlbumsList.jsx
+++ flask-photoalbum/client/src/components/AlbumsList.jsx
@@ -14,7 +14,9 @@ const AlbumsList = (props) => {
               key={album.id}
               className="container album"
             >
-              {album.title} - {album.description} - <Timestamp time={album.created_at} format='full' /> - <Timestamp time={album.created_at} format='ago' includeDay={true} precision={2} autoUpdate={60} /> - {album.images.length} files
+              <h2>{album.title}</h2>
+              <h6>{album.images.length} files - <Timestamp time={album.created_at} format='full' /> - <i><Timestamp time={album.created_at} format='ago' includeDay={true} precision={2} autoUpdate={60} /></i></h6>
+              <h5>{album.description}</h5>
               <div className="col-md-12">
                 <div className="row">
                   {
