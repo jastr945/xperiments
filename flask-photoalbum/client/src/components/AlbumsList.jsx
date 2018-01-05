9ea1ac744ff732f8da34e9c3487193290c2eec19
jastr945 <polina.jastr@gmail.com>
2018-01-04 18:51:13 -0800

    Added arrows

--- flask-photoalbum/client/src/components/AlbumsList.jsx
+++ flask-photoalbum/client/src/components/AlbumsList.jsx
@@ -17,13 +17,16 @@ const AlbumsList = (props) => {
               <h2>{album.title}</h2>
               <h6>{album.images.length} files - <Timestamp time={album.created_at} format='full' /> - <i><Timestamp time={album.created_at} format='ago' includeDay={true} precision={2} autoUpdate={60} /></i></h6>
               <h5>{album.description}</h5>
-              <div className="col-md-12">
+              <div className="slideshow col-md-12 text-center">
                 <div className="row">
+                  <div className="arrow col">
+                    <img src={require('./static/arrow-left.png')} width={50} alt="arrow" />
+                  </div>
                   {
                     album.images.map((i) => {
                       return (
                         <div
-                          className="image col-md-2"
+                          className="image col-md-2 text-center"
                           key={i.id}
                         >
                           <img src={i} alt='album img' width={200} height={120} />
@@ -31,6 +34,9 @@ const AlbumsList = (props) => {
                       )
                     })
                   }
+                  <div className="arrow col">
+                    <img src={require('./static/arrow-right.png')} width={50} alt="arrow" />
+                  </div>
                 </div>
               </div>
             </div>
