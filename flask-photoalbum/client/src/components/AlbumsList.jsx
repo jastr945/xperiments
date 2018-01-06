27a0ac9e71d6ca9e29676125d67407a609249c51
jastr945 <polina.jastr@gmail.com>
2018-01-05 21:09:58 -0800

    Gave images proper width and alignment

--- flask-photoalbum/client/src/components/AlbumsList.jsx
+++ flask-photoalbum/client/src/components/AlbumsList.jsx
@@ -17,26 +17,21 @@ const AlbumsList = (props) => {
               <h2>{album.title}</h2>
               <h6>{album.images.length} files - <Timestamp time={album.created_at} format='full' /> - <i><Timestamp time={album.created_at} format='ago' includeDay={true} precision={2} autoUpdate={60} /></i></h6>
               <h5>{album.description}</h5>
-              <div className="slideshow col-md-12 text-center">
-                <div className="row">
-                  <div className="arrow col">
-                    <img src={require('./static/arrow-left.png')} width={50} alt="arrow" />
-                  </div>
-                  {
-                    album.images.map((i) => {
-                      return (
-                        <div
-                          className="image col-md-2 text-center"
-                          key={i.id}
-                        >
-                          <img src={i} alt='album img' width={200} height={120} />
-                        </div>
-                      )
-                    })
-                  }
-                  <div className="arrow col">
-                    <img src={require('./static/arrow-right.png')} width={50} alt="arrow" />
-                  </div>
+              <div className="row">
+                <div className="arrow col-md-1 text-center">
+                  <img src={require('./static/arrow-left.png')} width={50} alt="arrow" />
+                </div>
+                <div className="allImages col-md-10 text-center">
+                {
+                  album.images.map((i) => {
+                    return (
+                      <img className="image" key={i.id} src={i} alt='album img' height={120} />
+                    )
+                  })
+                }
+                </div>
+                <div className="arrow col-md-1 text-center">
+                  <img src={require('./static/arrow-right.png')} width={50} alt="arrow" />
                 </div>
               </div>
             </div>
