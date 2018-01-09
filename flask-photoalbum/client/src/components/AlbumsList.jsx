cb153ef42305e78ff11846299041815e905bc6a3
jastr945 <polina.jastr@gmail.com>
2018-01-09 00:11:55 -0800

    Prevented multiple images from opening, added onMouseLeave function

--- flask-photoalbum/client/src/components/AlbumsList.jsx
+++ flask-photoalbum/client/src/components/AlbumsList.jsx
@@ -10,25 +10,38 @@ class AlbumsList extends React.Component {
     this.state = {
       albumID: -1,
       imgID: -1,
-      imgHovered: true,
+      imgHovered: false,
       imgClicked: false
     }
   }
   albumHover(albumindex) {
     this.setState({
       albumID: this.state.albumID === albumindex ? -1 : albumindex,
+      imgHovered: true
     });
   }
   imgHover(imgindex) {
     this.setState({
-      imgID: this.state.imgID === imgindex ? -1 : imgindex
+      imgID: this.state.imgID === imgindex ? -1 : imgindex,
+      imgHovered: true
     });
   }
-  openImg() {
-    this.setState({
-      imgHovered: false,
-      imgClicked: true
-    });
+  openImg(imgindex) {
+    var myindex = this.state.imgID;
+    if (imgindex === myindex) {
+      this.setState({
+        imgHovered: false,
+        imgClicked: true
+      });
+    }
+  }
+  imgMouseLeave(imgindex) {
+    var myindex = this.state.imgID;
+    if (imgindex === myindex) {
+      this.setState({
+        imgClicked: false
+      });
+    }
   }
   closeImg() {
     this.setState({
@@ -65,15 +78,14 @@ class AlbumsList extends React.Component {
                       var zoomedImg = (imgID === imgindex && albumID === albumindex && imgClicked === false && imgHovered === true) ? "zoomed" : "";
                       var openImg = (imgID === imgindex && albumID === albumindex && imgClicked === true && imgHovered === false) ? "opened" : "";
                       var imgClass = `imageContainer ${openImg} ${zoomedImg}`
-                      console.log(imgClass);
-                      console.log(imgClicked, imgHovered);
+                      console.log(imgID, albumID, imgClicked, imgHovered);
                       return (
                         <div className={imgClass} key={imgindex}>
                           {imgID === imgindex && albumID === albumindex && imgClicked === false && imgHovered === true && <img className="icon" src={require('./static/expand.png')} width={45} alt="arrow" />}
 
-                          {imgID === imgindex && albumID === albumindex && imgClicked === true && <img className="icon" onClick={this.closeImg.bind(this)} src="http://icons.iconarchive.com/icons/graphicloads/100-flat/256/close-icon.png" width={15} alt="close" />}
+                          {imgID === imgindex && albumID === albumindex && imgClicked === true && imgHovered === false && <img className="icon" onClick={this.closeImg.bind(this)} src="http://icons.iconarchive.com/icons/graphicloads/100-flat/256/close-icon.png" width={15} alt="close" />}
 
-                          <img className="image" onClick={this.openImg.bind(this)} onMouseEnter={this.imgHover.bind(this, imgindex)} src={i} alt='album img' />
+                          <img className="image" onClick={this.openImg.bind(this, imgindex)} onMouseEnter={this.imgHover.bind(this, imgindex)} onMouseLeave={this.imgMouseLeave.bind(this, imgindex)} src={i} alt='album img' />
                         </div>
                       )
                     })
