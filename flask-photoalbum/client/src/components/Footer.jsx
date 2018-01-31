253379247bdb322b1815d8ef4cd9545b7c24288c
jastr945 <polina.jastr@gmail.com>
2018-01-30 16:59:19 -0800

    Added automatic year calculation

--- flask-photoalbum/client/src/components/Footer.jsx
+++ flask-photoalbum/client/src/components/Footer.jsx
@@ -13,10 +13,8 @@ const Footer = (props) => {
         </ul>
       </div>
       <small>&copy;
-        <script type="text/javascript">
-          document.write(new Date().getFullYear());
-        </script>
-        &nbsp;jastr945; Icon made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></small>
+        {(new Date().getFullYear())}
+        &nbsp;Polina Jastrzebska; Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></small>
     </footer>
   )
 }
