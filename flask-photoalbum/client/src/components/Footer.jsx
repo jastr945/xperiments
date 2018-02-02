08394c423c147ccaa8b94666ab1cf44f7dc5a40d
jastr945 <polina.jastr@gmail.com>
2018-02-01 19:17:29 -0800

    Set up an image row component

--- flask-photoalbum/client/src/components/Footer.jsx
+++ flask-photoalbum/client/src/components/Footer.jsx
@@ -14,7 +14,7 @@ const Footer = (props) => {
       </div>
       <small>&copy;
         {(new Date().getFullYear())}
-        &nbsp;Polina Jastrzebska; Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></small>
+        &nbsp;Polina Jastrzebska; Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> are licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></small>
     </footer>
   )
 }
