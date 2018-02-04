cbc9491cf7c298177397ae2cef8af1a4f70dccc1
jastr945 <polina.jastr@gmail.com>
2018-02-04 15:58:30 -0800

    Added flask-oauthlib to views

--- flask-photoalbum/project/api/views.py
+++ flask-photoalbum/project/api/views.py
@@ -1,11 +1,11 @@
-from flask import Blueprint, jsonify, request, render_template, redirect
+from flask import Flask, jsonify
+from flask import Blueprint, jsonify, request, render_template, redirect, session, url_for
 from project.api.models import Album, Image
 from project import db
 from sqlalchemy import exc
 import datetime
 from flask_uploads import UploadSet, IMAGES, configure_uploads, patch_request_class
-from flask import Flask, jsonify
-
+from flask_oauthlib.client import OAuth
 
 # creating an instance of an app and configuring Flask-Uploads
 app = Flask(__name__)
@@ -14,12 +14,36 @@ photos = UploadSet('photos', IMAGES)
 configure_uploads(app, photos)
 patch_request_class(app, 32 * 1024 * 1024) # limits uploaded images to 32 megabytes
 
+# login with Google
+GOOGLE_CLIENT_ID = '418257197191-lpv9abrospnd232fbbaup896fvngikk6.apps.googleusercontent.com'
+GOOGLE_CLIENT_SECRET = 'xeWXWVM6CQdXUopgt-oTACeJ'
+oauth = OAuth(app)
+google = oauth.remote_app(
+    'google',
+    consumer_key=GOOGLE_CLIENT_ID,
+    consumer_secret=GOOGLE_CLIENT_SECRET,
+    request_token_params={
+        'scope': 'email'
+    },
+    base_url='https://www.googleapis.com/oauth2/v1/',
+    request_token_url=None,
+    access_token_method='POST',
+    access_token_url='https://accounts.google.com/o/oauth2/token',
+    authorize_url='https://accounts.google.com/o/oauth2/auth',
+)
+
 # creating a blueprint
 albums_blueprint = Blueprint('albums', __name__)
 
 
 @albums_blueprint.route('/', methods=['GET', 'POST'])
 def index():
+    # Google authorization
+    if 'google_token' in session:
+        me = google.get('userinfo')
+        return jsonify({"data": me.data})
+    return redirect(url_for('login'))
+
     if request.method == 'POST':
         title = request.form['title']
         description = request.form['description']
@@ -29,6 +53,31 @@ def index():
         db.session.commit()
     albums = Album.query.order_by(Album.created_at.desc()).all()
     return render_template('index.html', albums=albums)
+
+@albums_blueprint.route('/login')
+def login():
+    return google.authorize(callback=url_for('authorized', _external=True))
+
+@albums_blueprint.route('/logout')
+def logout():
+    session.pop('google_token', None)
+    return redirect(url_for('index'))
+
+@albums_blueprint.route('/login/authorized')
+def authorized():
+    resp = google.authorized_response()
+    if resp is None:
+        return 'Access denied: reason=%s error=%s' % (
+            request.args['error_reason'],
+            request.args['error_description']
+        )
+    session['google_token'] = (resp['access_token'], '')
+    me = google.get('userinfo')
+    return jsonify({"data": me.data})
+
+@google.tokengetter
+def get_google_oauth_token():
+    return session.get('google_token')
 
 @albums_blueprint.route('/ping', methods=['GET'])
 def ping_pong():
