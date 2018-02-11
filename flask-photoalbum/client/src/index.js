ed491a1a5e64e65c3a08d5244a223f8e3e6d43c5
jastr945 <polina.jastr@gmail.com>
2018-02-10 22:46:38 -0800

    Fixed bug in index.js

--- flask-photoalbum/client/src/index.js
+++ flask-photoalbum/client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 import App from './App.jsx';
 
-ReactDOM.render((
+ReactDOM.render(
   <App />,
-  document.getElementById('root'))
+  document.getElementById('root')
 );
