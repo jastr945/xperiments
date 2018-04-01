d4f3f638e329fa6b7a2a24d4bb151e3a08912c8a
jastr945 <polina.jastr@gmail.com>
2018-04-01 16:38:49 -0700

    Added the Item model, edited the form, added initial POST request to views

--- djangokube/kubebooks/booksapp/models.py
+++ djangokube/kubebooks/booksapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    """Contains basic information about each book or movie."""
+
+    itemtype = models.CharField(max_length=255, default='', null=False)
+    name = models.CharField(max_length=255, default='', null=False)
+    author = models.CharField(max_length=255, default='', null=False)
--- djangokube/kubebooks/booksapp/views.py
+++ djangokube/kubebooks/booksapp/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from .models import Item
 
 def index(request):
-    return render(request, 'booksapp/index.html')
+    try:
+        items = Item.objects.all()
+        if request.POST:
+            itemtype=request.POST['itemtype']
+            title=request.POST['title']
+            author=request.POST['author']
+            import ipdb; ipdb.set_trace()
+    except ValueError:
+        pass
+    return render(request, 'booksapp/index.html', {"items": items})
