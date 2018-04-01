3e2d724e73718e0508daeb46302f43d81a97f4c0
jastr945 <polina.jastr@gmail.com>
2018-04-01 14:06:44 -0700

    Added templates and index view

--- djangokube/kubebooks/kubebooks/urls.py
+++ djangokube/kubebooks/kubebooks/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from booksapp import views as page_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^', page_views.index, name='index'),
 ]
