a9240f6d44b0b5391491e6b3b13c964982c4a9a2
jastr945 <polina.jastr@gmail.com>
2018-04-06 19:10:24 -0700

    Changed allowed hosts and kubernetes files

--- djangokube/kubebooks/kubebooks/settings.py
+++ djangokube/kubebooks/kubebooks/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+from socket import gethostname, gethostbyname
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'k)b&hy@rl69deygl#iizpuizu+$ars
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = ['172.17.0.3', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
