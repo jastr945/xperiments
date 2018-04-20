6d27f88ec06978fce79fc3e1e37aee5b381af21c
jastr945 <polina.jastr@gmail.com>
2018-04-19 20:13:06 -0700

    Added a delete view

--- shopping_list_api/app.py
+++ shopping_list_api/app.py
@@ -20,24 +20,19 @@ items = [
 
 @app.errorhandler(404)
 def not_found(error):
+    """Handling 404 nicely"""
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 @app.route('/api/v1.0/items', methods=['GET'])
 def get_items():
+    """Listing all items"""
     return jsonify({'items': items})
-
-
-@app.route('/api/v1.0/items/<int:item_id>', methods=['GET'])
-def get_item(item_id):
-    item = [item for item in items if item['id'] == item_id]
-    if len(item) == 0:
-        abort(404)
-    return jsonify({'item': item[0]})
 
 
 @app.route('/api/v1.0/items', methods=['POST'])
 def add_item():
+    """Adding an item to the list"""
     if not request.json or not 'title' in request.json:
         abort(400)
     item = {
@@ -48,6 +43,33 @@ def add_item():
     }
     items.append(item)
     return jsonify({'item': item}), 201
+
+
+@app.route('/api/v1.0/items/<int:item_id>', methods=['GET'])
+def get_item(item_id):
+    """Getting a single item"""
+    item = [item for item in items if item['id'] == item_id]
+    if len(item) == 0:
+        abort(404)
+    return jsonify({'item': item[0]})
+
+
+@app.route('/api/v1.0/items/<int:item_id>', methods=['PUT'])
+def update_item(item_id):
+    """Updating a single item"""
+    pass
+
+
+
+@app.route('/api/v1.0/items/<int:item_id>', methods=['DELETE'])
+def delete_item(item_id):
+    """Deleting a single item"""
+    item = [item for item in items if item['id'] == item_id]
+    if len(item) == 0:
+        abort(404)
+    else:
+        items.remove(item[0])
+    return jsonify({'result': True})
 
 
 @app.route('/')
