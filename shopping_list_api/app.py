e55e92cb3f334105e2dd685c3c554301aebcec58
jastr945 <polina.jastr@gmail.com>
2018-04-19 19:31:53 -0700

    Added error handling

--- shopping_list_api/app.py
+++ shopping_list_api/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, abort, make_response
 
 app = Flask(__name__)
 
@@ -17,9 +17,23 @@ items = [
     }
 ]
 
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
 @app.route('/api/v1.0/items', methods=['GET'])
-def get_tasks():
+def get_items():
     return jsonify({'items': items})
+
+
+@app.route('/api/v1.0/items/<int:item_id>', methods=['GET'])
+def get_item(item_id):
+    item = [item for item in items if item['id'] == item_id]
+    if len(item) == 0:
+        abort(404)
+    return jsonify({'item': item[0]})
 
 
 @app.route('/')
