ab087a70dbf2041cc1835d778a061d0529031203
jastr945 <polina.jastr@gmail.com>
2018-04-20 22:12:43 -0700

    Added js function deleting placeholder on click

--- shopping_list_api/static/site.js
+++ shopping_list_api/static/site.js
@@ -1,25 +1,34 @@
 'use strict';
 
-// Adding an item to the list
-$('#addItem').click(function (e) {
-  e.preventDefault();
-  var href = "api/v1.0/items";
-  var form_data = $('form').serialize();
-  $.ajax({
-    url: href,
-    type: "POST",
-    dataType: "json",
-    processData: false,
-    contentType: 'application/json',
-    data: form_data,
-    success: function(data){
-      console.log("Item posted" + data);
-    },
-    error: function( jqXhr, textStatus, errorThrown ){
-        console.log( errorThrown );
-    }
-  })
+// removing placeholder from the search tab on focus and bringing it back on blur
+$(document).ready(function() {
+  $('.form-control').focus(function(){
+     $(this).data('placeholder', $(this).attr('placeholder'))
+      .attr('placeholder','');
+    }).blur(function(){
+      $(this).attr('placeholder', $(this).data('placeholder'));
+  });
 });
+// Adding an item to the list
+// $('#addItem').click(function (e) {
+//   e.preventDefault();
+//   var href = "api/v1.0/items";
+//   var form_data = $('form').serialize();
+//   $.ajax({
+//     url: href,
+//     type: "POST",
+//     dataType: "json",
+//     processData: false,
+//     contentType: 'application/json',
+//     data: form_data,
+//     success: function(data){
+//       console.log("Item posted" + data);
+//     },
+//     error: function( jqXhr, textStatus, errorThrown ){
+//         console.log( errorThrown );
+//     }
+//   })
+// });
 
 // Removes an entry from the list upon clicking on the cross icon
 // $('.deleteButton').each(function () {
