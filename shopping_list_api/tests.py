314bf693c325e5fc8f20ffe90b3a428189507b9d
jastr945 <polina.jastr@gmail.com>
2018-04-29 22:13:29 -0700

    More tests

--- shopping_list_api/tests.py
+++ shopping_list_api/tests.py
@@ -14,19 +14,52 @@ class BasicTests(TestCase):
         project_dir = os.path.dirname(os.path.abspath(__file__))
         database_file = "sqlite:///{}".format(os.path.join(project_dir, "test.db"))
         self.app = app.test_client()
+        self.app_context = app.app_context()
+        self.app_context.push()
         db.create_all()
 
     def tearDown(self):
         db.session.remove()
         db.drop_all()
+        self.app_context.pop()
 
-    # def test_get_items(self):
-    #     response = self.app.get('/api/v1.0/items')
-    #     assert response.status_code, 200
-    #
-    # def test_get_users(self):
-    #     response = self.app.get('/api/v1.0/users')
-    #     assert response.status_code, 200
+    def test_user(self):
+        u = User(name='user1')
+        u.hash_password('password')
+        db.session.add(u)
+        db.session.commit()
+        myuser = User.query.filter_by(id='1').first()
+        assert myuser.name=='user1'
+
+    def test_fail_user(self):
+        u = User(name='user1')
+        u.hash_password('password')
+        db.session.add(u)
+        db.session.commit()
+        myuser = User.query.filter_by(id='1').first()
+        assert myuser.name=='user2'
+
+    def test_item(self):
+        i = Item(title='test title', note='test note', user_id='1')
+        db.session.add(i)
+        db.session.commit()
+        myitem = Item.query.filter_by(id='1').first()
+        assert myitem.title=='test title'
+
+    def test_fail_item(self):
+        i = Item(title='test title', note='test note', user_id='1')
+        db.session.add(i)
+        db.session.commit()
+        myitem = Item.query.filter_by(id='1').first()
+        assert myitem.note=='fail note'
+
+    def test_get_items(self):
+        response = self.app.get('/api/v1.0/items')
+        assert response.status_code==200
+
+    def test_get_users(self):
+        response = self.app.get('/api/v1.0/users')
+        assert response.status_code==200
 
 
 # with app.test_client() as c:
@@ -35,3 +68,6 @@ class BasicTests(TestCase):
 #     })
 #     json_data = rv.get_json()
 #     assert verify_token(email, json_data['token'])
+
+if __name__ == "__main__":
+    unittest.main()
