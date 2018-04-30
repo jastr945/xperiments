78daa98dab1743955e24a42f28bfeb29c43247e8
jastr945 <polina.jastr@gmail.com>
2018-04-29 21:06:31 -0700

    Added setUp and tearDown to tests

--- shopping_list_api/tests.py
+++ shopping_list_api/tests.py
@@ -1,15 +1,32 @@
-from app import app
+import os
+from flask import Flask
 import unittest
 from unittest import TestCase
+from app import app
+from models import db
+from models import Item, User
 
-class TestIntegrations(unittest.TestCase):
+
+class BasicTests(TestCase):
     def setUp(self):
+        app.config['TESTING'] = True
+        app.config['WTF_CSRF_ENABLED'] = False
+        project_dir = os.path.dirname(os.path.abspath(__file__))
+        database_file = "sqlite:///{}".format(os.path.join(project_dir, "test.db"))
         self.app = app.test_client()
+        db.create_all()
 
-    def test_thing(self):
-        response = self.app.get('/api/v1.0/items')
-        print(response.data)
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
 
+    # def test_get_items(self):
+    #     response = self.app.get('/api/v1.0/items')
+    #     assert response.status_code, 200
+    #
+    # def test_get_users(self):
+    #     response = self.app.get('/api/v1.0/users')
+    #     assert response.status_code, 200
 
 
 # with app.test_client() as c:
