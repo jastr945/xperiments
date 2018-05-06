bf1a11940f98c79aee1f11bebec794c04874a370
jastr945 <polina.jastr@gmail.com>
2018-05-06 15:17:34 -0700

    Workshop list problem solved

--- algorithms/lists_manipulation.py
+++ algorithms/lists_manipulation.py
@@ -1,9 +1,17 @@
-# a=[1,2,3,4,5,6,7,8,9]
-#
-# print(a[::-1])
-# print(a[::2])
+# example input [1, 2, 3, 5], output: 4
+def find_missing(my_list):
+    """Returns a missing integer in a given list"""
+    for index, elem in enumerate(my_list):
+        ref = my_list[0]
+        diff = elem - index
+        results = []
+        if ref!=diff:
+            results.append(my_list[index] - 1)
+            result = results[0]
+            return result
 
-
-rec = {"Name" : "Python", "Age":"20", "Addr" : "NJ", "Country" : "USA"}
-id1 = id(rec)
-print(id1)
+print(find_missing([3, 4, 5, 7])) #6
+print(find_missing([0, 1, 3, 4])) #2
+print(find_missing([0, 1, 2, 3, 4, 6])) #5
+print(find_missing([3, 4, 5, 6, 7, 9])) #8
+print(find_missing([0, 2, 3, 4, 5, 6])) #1
