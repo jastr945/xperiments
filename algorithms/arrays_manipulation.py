f6d5ad53da582921f58caa0edd996feb6757c13a
jastr945 <polina.jastr@gmail.com>
2018-05-09 15:27:32 -0700

    List exercise

--- algorithms/arrays_manipulation.py
+++ algorithms/arrays_manipulation.py
@@ -2,3 +2,15 @@
 my_array = ["1", "0", "x", "a", "c"]
 my_array.sort()
 print(my_array)
+
+
+# removing duplicates from a list
+def remove_dupes(my_list):
+    """Iterating over a given list and returning a new list without duplicates"""
+    new_list = []
+    for i in my_list:
+        if i not in new_list:
+            new_list.append(i)
+    return new_list
+
+print(remove_dupes(['9', '3', '9', '0', '0', '1']))
