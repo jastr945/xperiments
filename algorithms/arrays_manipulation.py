9dee7651899f84029e432882a30a31ae6a057fcf
jastr945 <polina.jastr@gmail.com>
2018-05-09 18:01:07 -0700

    Linked list attempt

--- algorithms/arrays_manipulation.py
+++ algorithms/arrays_manipulation.py
@@ -14,3 +14,27 @@ def remove_dupes(my_list):
     return new_list
 
 print(remove_dupes(['9', '3', '9', '0', '0', '1']))
+
+
+# Traversing a linked list
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+    def traverse(self):
+        node = self
+        while node != None:
+            print(node.val)
+            node = node.next
+
+if __name__ == "__main__":
+    node1 = Node(1)
+    node2 = Node(2)
+    node3 = Node(3)
+    node4 = Node(1)
+    node1.next = node2
+    node2.next = node3
+    node3.next = node4
+
+    node1.traverse()
